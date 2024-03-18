a5b622012981396c675a4e636cd84f1cfd7f1d2a
PrannavP <ramsarilekhi@gmail.com>
2024-03-18 10:00:15 +0545

    Implement full CRUD functionality for MongoDB API

--- api/src/index.js
+++ api/src/index.js
@@ -15,13 +15,15 @@ app.use(express.json());
 
 // default get method
 app.get('/', async function(req, res){
-    const students = await Student.find();
+    const students = await Student.find({});
     res.json(students);
 });
 
 // get method with id
-app.get('/:id', (req, res) => {
-    res.send('Get student by id.')
+app.get('/:id', async (req, res) => {
+    let id = req.params.id;
+    const sdata = await Student.findById(id);
+    res.json(sdata);
 });
 
 // post method
@@ -37,8 +39,16 @@ app.put('/', (req, res) => {
     res.send('I am put method');
 });
 
-app.delete('/', (req, res) => {
-    res.send('I am delete method');
+app.put('/:id', async (req, res) => {
+    let id = req.params.id;
+    await Student.findByIdAndUpdate(id, req.body);
+    res.json({ status: 200, message: "Student updated" });
+});
+
+app.delete('/:id', async (req, res) => {
+    let id = req.params.id;
+    await Student.findByIdAndDelete(id);
+    res.json({ status: 200, message: "Student deleted" });
 });
 
 app.listen(3000, function(){
